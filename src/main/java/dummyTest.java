2c6de9633fec507eeebad646f24e44d59e4a32e1
vikasharitwal93 <vikas.haritwal@harness.io>
2023-04-12 13:37:10 +0530

    UT

--- src/main/java/dummyTest.java
+++ src/main/java/dummyTest.java
@@ -3,4 +3,9 @@ public class dummyTest {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public boolean stringComp(String str1,String str2)
+    {
+        return str1.equals(str2);
+    }
 }
---
+++ src/test/java/sampleUnitTes.java
@@ -0,0 +1,18 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class sampleUnitTes {
+
+    @Test
+    public void t1()
+    {
+        Assertions.assertTrue(new dummyTest().stringComp("RAM","RAM"));
+    }
+
+    @Test
+    public void t6()
+    {
+        Assertions.assertTrue("RAM"!="RAM");
+    }
+}
