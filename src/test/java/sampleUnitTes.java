5c90fca7476b996178ebb4fe79fd68bc8399820b
vikasharitwal93 <vikas.haritwal@harness.io>
2023-04-13 12:22:02 +0530

    Added CheckStyle failure

--- src/test/java/sampleUnitTes.java
+++ src/test/java/sampleUnitTes.java
@@ -2,12 +2,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import java.util.*;
+
+
 public class sampleUnitTes {
 
     @Test
     public void t1()
     {
         Assertions.assertTrue(new dummyTest().stringComp("RAM","RAM"));
+
+
     }
 
     @Test
